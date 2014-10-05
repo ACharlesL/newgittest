ee0a1086a5b7d7f880c5a0b531ee3717b8f3c471
albert lewis <albertlewis@Macintosh.local>
2014-10-05 16:14:23 -0400

    species demo

---
+++ SpeciesFirstTryDemo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package textBookExcercise;
+
+/**
+ *
+ * @author albertlewis
+ */
+public class SpeciesFirstTryDemo {
+    public static void main(String[] args)
+    {
+        SpeciesFirstTry speciesOfTheMonth = new SpeciesFirstTry();
+        System.out.println("Enter data on the Species of "+ "the month:");
+    }
+    
+}
